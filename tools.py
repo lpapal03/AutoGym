8b689607d06c4f9acf678c42a07836a3aa4a77a6
lpapal03 <62185335+lpapal03@users.noreply.github.com>
2023-01-17 20:12:13 +0200

    Update tools.py

--- tools.py
+++ tools.py
@@ -15,18 +15,18 @@ def extract_details(filename):
     return username.replace('\n', ''), password.replace('\n', ''), preference_map
 
 
-def caluclate_resv_date_and_time(preference_map):
+def caluclate_resv_date_and_time(username, preference_map):
     # calculate date to reserve
     today = datetime.now()
-    target_date = today + timedelta(days=5)
+    target_date = today + timedelta(days=2)
     resv_date = target_date.strftime("%d-%m-%Y")
 
     # if target day is Sunday or has no preference then end
     if target_date.weekday() == 6:
-        print('Target day is Sunday')
+        print(username + ' - Target day is Sunday')
         return '', '', True
     if str(target_date.weekday()) not in preference_map.keys():
-        print('Target day not in preference map')
+        print(username + ' - Target day not in preference map')
         return '', '', True
     resv_time = preference_map[str(target_date.weekday())]
 
